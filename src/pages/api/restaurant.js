61ef589e218bbeab2daf1f8164188c5dc73c792c
niccolo <niccolosakko92@gmail.com>
2024-09-16 00:33:12 +0200

    fix comma

--- src/pages/api/restaurant.js
+++ src/pages/api/restaurant.js
@@ -7,7 +7,6 @@ export default async function handler (request, response) {
       const final = restaurants.rows[0]
       response.status(200).json({ restaurants: [final] })
     } catch (err) {
-      console.log(err,'ciao')
       response.status(500).send({ message: ['Get not available'], err })
     }
   }
